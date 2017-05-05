a7df9356166bc908ff3e093b7074f3c7adfbc8ee
Andea Balducci <mtb.snowboard@gmail.com>
2017-05-05 07:34:05 +0200

    Added Tape tests

---
+++ src/NStore.Tests/Persistence/TapeTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NStore.Raw;
+using Xunit;
+
+namespace NStore.Tests.Persistence
+{
+    public class TapeTests
+    {
+        [Fact]
+        public void new_tape_is_empty()
+        {
+            var tape = new Tape();
+
+            Assert.True(tape.IsEmpty);
+        }
+
+        [Fact]
+        public void record()
+        {
+            var tape = new Tape();
+
+            tape.Record(1, "a");
+
+            Assert.False(tape.IsEmpty);
+            Assert.Equal(1, tape.Length);
+            Assert.Equal("a", tape[0]);
+        }
+
+        [Fact]
+        public void replay()
+        {
+            var tape = new Tape();
+
+            tape.Record(1, "a");
+
+            var list = new List<object>();
+            tape.Replay(list.Add);
+
+            Assert.Equal(1, list.Count);
+            Assert.Equal("a", list[0]);
+        }
+    }
+}
