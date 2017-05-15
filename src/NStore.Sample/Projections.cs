a7e65aa9cbe9d222af551c5211d33433c854a031
Andrea Balducci <mtb.snowboard@gmail.com>
2017-05-15 18:37:41 +0200

    moved projection to new file

--- src/NStore.Sample/Projections.cs
+++ src/NStore.Sample/Projections.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using NStore.Aggregates;
 using NStore.Raw;
-using NStore.Sample.Domain.Room;
 
 namespace NStore.Sample
 {
@@ -47,23 +46,6 @@ namespace NStore.Sample
         private void Setup()
         {
             _projections.Add(Rooms);
-        }
-    }
-
-    public class RoomsOnSaleProjection : AbstractProjector
-    {
-        public class RoomsOnSale
-        {
-            public string Id { get; set; }
-            public string RoonNumber { get; set; }
-        }
-        private IDictionary<string, RoomsOnSale> _all = new Dictionary<string, RoomsOnSale>();
-
-        public IEnumerable<RoomsOnSale> List => _all.Values;
-
-        public void On(RoomMadeAvailable e)
-        {
-            _all.Add(e.Id, new RoomsOnSale { Id = e.Id });
         }
     }
 }
---
+++ src/NStore.Sample/RoomsOnSaleProjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NStore.Aggregates;
+using NStore.Sample.Domain.Room;
+
+namespace NStore.Sample
+{
+    public class RoomsOnSaleProjection : AbstractProjector
+    {
+        public class RoomsOnSale
+        {
+            public string Id { get; set; }
+            public string RoonNumber { get; set; }
+        }
+        private IDictionary<string, RoomsOnSale> _all = new Dictionary<string, RoomsOnSale>();
+
+        public IEnumerable<RoomsOnSale> List => _all.Values;
+
+        public void On(RoomMadeAvailable e)
+        {
+            _all.Add(e.Id, new RoomsOnSale { Id = e.Id });
+        }
+    }
+}
