03779875c5c2c2cf598b5f687442bbf1a7664f1f
Andea Balducci <mtb.snowboard@gmail.com>
2017-05-16 22:16:22 +0200

    added tests on sample app

---
+++ src/NStore.Sample/Domain/Room/DateRange.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NStore.Sample.Domain.Room
+{
+    public class DateRange
+    {
+        public DateTime From { get; }
+        public DateTime To { get; }
+
+        public DateRange(DateTime from, DateTime to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+
+        public bool Overlaps(DateRange range)
+        {
+            return this.Equals(range) ||
+                   this.From < range.To && this.To > range.From;
+        }
+
+        protected bool Equals(DateRange other)
+        {
+            return From.Equals(other.From) && To.Equals(other.To);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DateRange) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (From.GetHashCode() * 397) ^ To.GetHashCode();
+            }
+        }
+    }
+}
