eef88ce0a137fef5293f117e33749d011e098fee
Andrea Balducci <mtb.snowboard@gmail.com>
2017-05-28 17:14:49 +0200

    cleanup

--- src/NStore/Streams/IReadOnlyStream.cs
+++ src/NStore/Streams/IReadOnlyStream.cs
@@ -8,31 +8,28 @@ namespace NStore.Streams
     public interface IReadOnlyStream
     {
         Task Read(
-            IPartitionConsumer partitionConsumer
-        );
-        
-        Task Read(
-            IPartitionConsumer partitionConsumer, 
-            int fromIndexInclusive
-        );
-
-        Task Read(
-            IPartitionConsumer partitionConsumer, 
-            int fromIndexInclusive, 
-            CancellationToken cancellationToken
-        );  
-
-        Task Read(
-            IPartitionConsumer partitionConsumer, 
-            int fromIndexInclusive, 
-            int toIndexInclusive 
-        );
-        
-        Task Read(
-            IPartitionConsumer partitionConsumer, 
-            int fromIndexInclusive, 
-            int toIndexInclusive, 
+            IPartitionConsumer partitionConsumer,
+            int fromIndexInclusive,
+            int toIndexInclusive,
             CancellationToken cancellationToken
         );
+    }
+
+    public static class ReadOnlyStreamExtensions
+    {
+        public static Task Read(this IReadOnlyStream stream, IPartitionConsumer partitionConsumer)
+        {
+            return stream.Read(partitionConsumer, 0, int.MaxValue, CancellationToken.None);
+        }
+
+        public static Task Read(this IReadOnlyStream stream, IPartitionConsumer partitionConsumer, int fromIndexInclusive)
+        {
+            return stream.Read(partitionConsumer, fromIndexInclusive, int.MaxValue, CancellationToken.None);
+        }
+
+        public static Task Read(this IReadOnlyStream stream, IPartitionConsumer partitionConsumer, int fromIndexInclusive, int toIndexInclusive)
+        {
+            return stream.Read(partitionConsumer, fromIndexInclusive, toIndexInclusive, CancellationToken.None);
+        }
     }
 }
--- src/NStore/Streams/Stream.cs
+++ src/NStore/Streams/Stream.cs
@@ -17,26 +17,6 @@ namespace NStore.Streams
             this.Raw = raw;
         }
 
-        public Task Read(IPartitionConsumer partitionConsumer)
-        {
-            return Read(partitionConsumer, 0, int.MaxValue, default(CancellationToken));
-        }
-        
-        public Task Read(IPartitionConsumer partitionConsumer, int fromIndexInclusive)
-        {
-            return Read(partitionConsumer, fromIndexInclusive, int.MaxValue, default(CancellationToken));
-        }
-
-        public Task Read(IPartitionConsumer partitionConsumer, int fromIndexInclusive, CancellationToken cancellationToken)
-        {
-            return Read(partitionConsumer, fromIndexInclusive, int.MaxValue, cancellationToken);
-        }
-
-        public Task Read(IPartitionConsumer partitionConsumer, int fromIndexInclusive, int toIndexInclusive)
-        {
-            return Read(partitionConsumer, fromIndexInclusive, toIndexInclusive, default(CancellationToken));
-        }
-
         public Task Read(IPartitionConsumer partitionConsumer, int fromIndexInclusive, int toIndexInclusive, CancellationToken cancellationToken)
         {
             return Raw.ReadPartitionForward(
@@ -44,34 +24,14 @@ namespace NStore.Streams
                 fromIndexInclusive,
                 partitionConsumer,
                 toIndexInclusive,
-                limit:Int32.MaxValue,
-                cancellationToken: cancellationToken
+                int.MaxValue,
+                cancellationToken
             );
-        }
-
-        public Task Append(object payload)
-        {
-            return Append(payload, null, default(CancellationToken));
-        }
-
-        public Task Append(object payload, CancellationToken cancellation)
-        {
-            return Append(payload, null, cancellation);
-        }
-
-        public Task Append(object payload, string operationId)
-        {
-            return Append(payload, operationId, default(CancellationToken));
         }
  
         public virtual Task Append(object payload, string operationId, CancellationToken cancellation)
         {
             return Raw.PersistAsync(this.Id, -1, payload, operationId, cancellation);
-        }
-
-        public Task Delete()
-        {
-            return Delete(default(CancellationToken));
         }
 
         public virtual Task Delete(CancellationToken cancellation)
