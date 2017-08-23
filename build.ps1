15157d788381a24845c9a0e57fffc2999f1aa6ab
Alkampfergit <ricci.gianmaria@gmail.com>
2017-08-23 11:19:13 +0200

    updated buil.ps1 to dump cake commandline

--- build.ps1
+++ build.ps1
@@ -185,5 +185,6 @@ if (!(Test-Path $CAKE_EXE)) {
 
 # Start Cake
 Write-Host "Running build script..."
+Write-Output "Invoking Cake.exe with commandline: & `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
 Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
 exit $LASTEXITCODE
