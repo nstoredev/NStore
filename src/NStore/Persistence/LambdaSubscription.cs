1f0f28d450a2bd11fa46f4e044a48573ccab17a5
Andrea Balducci <mtb.snowboard@gmail.com>
2017-08-24 12:36:59 +0200

    added events on lambdasubscription

--- src/NStore/Persistence/LambdaSubscription.cs
+++ src/NStore/Persistence/LambdaSubscription.cs
@@ -4,14 +4,31 @@ using System.Threading.Tasks;
 
 namespace NStore.Persistence
 {
+
     public class LambdaSubscription : ISubscription
     {
+        public delegate Task OnErrorDelegate(long position, Exception ex);
+        public delegate Task OnStartDelegate(long position);
+        public delegate Task OnCompleteDelegate(long position);
+        public delegate Task OnStopDelegate(long position);
+
+
         private readonly ChunkProcessor _fn;
         private Exception _failed;
+        private long _failedPosition;
+
+
         public bool ReadCompleted { get; private set; }
         public bool Failed => this._failed != null;
+        public long FailedPosition => _failedPosition;
         public Exception LastError => _failed;
-        
+
+
+        public OnErrorDelegate OnError { get; set; }
+        public OnStartDelegate OnStart { get; set; }
+        public OnCompleteDelegate OnComplete { get; set; }
+        public OnStopDelegate OnStop { get; set; }
+
         public LambdaSubscription(ChunkProcessor fn)
         {
             _fn = fn;
@@ -24,25 +41,36 @@ namespace NStore.Persistence
 
         public Task OnStartAsync(long position)
         {
-            return Task.CompletedTask;
+            return OnStart != null
+                ? OnStart(position)
+                : Task.CompletedTask;
         }
 
         public Task CompletedAsync(long position)
         {
             this.ReadCompleted = true;
-            return Task.CompletedTask;
+
+            return OnComplete!= null
+                ? OnComplete(position)
+                : Task.CompletedTask;
         }
 
         public Task StoppedAsync(long position)
         {
             this.ReadCompleted = true;
-            return Task.CompletedTask;
+            return OnStop != null
+                ? OnStop(position)
+                : Task.CompletedTask;
         }
 
         public Task OnErrorAsync(long position, Exception ex)
         {
             _failed = ex;
-            return Task.CompletedTask;
+            _failedPosition = position;
+
+            return OnError != null
+                ? OnError(position, ex)
+                : Task.CompletedTask;
         }
     }
 }
