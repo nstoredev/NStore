3cb0f5c36e37ce47606e440cc4fcbd82181b2843
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-10-25 09:38:22 +0200

    Added afterinit to base aggregate class
    This is needed to allow for post initialization stuff, and because
    it is the only moment when we are sure that the state was
    recreated.

--- src/NStore.Domain/Aggregate.cs
+++ src/NStore.Domain/Aggregate.cs
@@ -6,144 +6,152 @@ using NStore.Core.Snapshots;
 
 namespace NStore.Domain
 {
-    public abstract class Aggregate<TState> :
-        IEventSourcedAggregate,
-        ISnaphottable,
-        IAggregate
-        where TState : class, new()
-    {
-        public string Id { get; private set; }
-        public long Version { get; private set; }
-        public bool IsInitialized { get; private set; }
+	public abstract class Aggregate<TState> :
+		IEventSourcedAggregate,
+		ISnaphottable,
+		IAggregate
+		where TState : class, new()
+	{
+		public string Id { get; private set; }
+		public long Version { get; private set; }
+		public bool IsInitialized { get; private set; }
 
-        private IList<object> PendingChanges { get; } = new List<object>();
-        protected TState State { get; private set; }
-        public bool IsDirty => this.PendingChanges.Any();
-        public bool IsNew => this.Version == 0;
-        private readonly IPayloadProcessor _processor;
+		private IList<object> PendingChanges { get; } = new List<object>();
+		protected TState State { get; private set; }
+		public bool IsDirty => this.PendingChanges.Any();
+		public bool IsNew => this.Version == 0;
+		private readonly IPayloadProcessor _processor;
 
-        protected Aggregate() : this((IPayloadProcessor)null)
-        {
+		protected Aggregate() : this((IPayloadProcessor)null)
+		{
 
-        }
+		}
 
-        protected Aggregate(IPayloadProcessor processor)
-        {
-            _processor = processor ?? DelegateToPrivateEventHandlers.Instance;
-        }
+		protected Aggregate(IPayloadProcessor processor)
+		{
+			_processor = processor ?? DelegateToPrivateEventHandlers.Instance;
+		}
 
-        protected virtual string StateSignature => "1";
+		protected virtual string StateSignature => "1";
 
-        public void Init(string id) => InternalInit(id, 0, null);
+		public void Init(string id) => InternalInit(id, 0, null);
 
-        private void InternalInit(string aggregateId, long aggregateVersion, TState state)
-        {
-            if (String.IsNullOrEmpty(aggregateId))
-                throw new ArgumentNullException(nameof(aggregateId));
+		private void InternalInit(string aggregateId, long aggregateVersion, TState state)
+		{
+			if (String.IsNullOrEmpty(aggregateId))
+				throw new ArgumentNullException(nameof(aggregateId));
 
-            if (this.Id != null)
-                throw new AggregateAlreadyInitializedException(GetType(), this.Id);
+			if (this.Id != null)
+				throw new AggregateAlreadyInitializedException(GetType(), this.Id);
 
-            this.Id = aggregateId;
-            this.State = state ?? new TState();
-            this.IsInitialized = true;
-            this.PendingChanges.Clear();
-            this.Version = aggregateVersion;
-        }
+			this.Id = aggregateId;
+			this.State = state ?? new TState();
+			this.IsInitialized = true;
+			this.PendingChanges.Clear();
+			this.Version = aggregateVersion;
 
-        bool ISnaphottable.TryRestore(SnapshotInfo snapshotInfo)
-        {
-            if (snapshotInfo == null) throw new ArgumentNullException(nameof(snapshotInfo));
+			this.AfterInit();
+		}
 
-            var processed = PreprocessSnapshot(snapshotInfo);
+		protected virtual void AfterInit()
+		{
+			//Do nothing after init, but we left the ability to derived classes 
+			//to do something after they are initialized
+		}
 
-            if (processed == null || processed.IsEmpty)
-                return false;
+		bool ISnaphottable.TryRestore(SnapshotInfo snapshotInfo)
+		{
+			if (snapshotInfo == null) throw new ArgumentNullException(nameof(snapshotInfo));
 
-            this.InternalInit(
-                processed.SourceId,
-                processed.SourceVersion,
-                (TState)processed.Payload
-            );
+			var processed = PreprocessSnapshot(snapshotInfo);
 
-            return true;
-        }
+			if (processed == null || processed.IsEmpty)
+				return false;
 
-        protected virtual void PostLoadingProcessing()
-        {
-            // entry point for custom logic on load
-        }
+			this.InternalInit(
+				processed.SourceId,
+				processed.SourceVersion,
+				(TState)processed.Payload
+			);
 
-        /// <summary>
-        /// Give chance to upcast state or just drop snapshot with empty default state
-        /// </summary>
-        /// <param name="snapshotInfo"></param>
-        /// <returns></returns>
-        protected virtual SnapshotInfo PreprocessSnapshot(SnapshotInfo snapshotInfo)
-        {
-            return snapshotInfo;
-        }
+			return true;
+		}
 
-        SnapshotInfo ISnaphottable.GetSnapshot()
-        {
-            return new SnapshotInfo(
-                this.Id,
-                this.Version,
-                this.State,
-                this.StateSignature
-            );
-        }
+		protected virtual void PostLoadingProcessing()
+		{
+			// entry point for custom logic on load
+		}
 
-        void IEventSourcedAggregate.Loaded()
-        {
-            PostLoadingProcessing();
-        }
+		/// <summary>
+		/// Give chance to upcast state or just drop snapshot with empty default state
+		/// </summary>
+		/// <param name="snapshotInfo"></param>
+		/// <returns></returns>
+		protected virtual SnapshotInfo PreprocessSnapshot(SnapshotInfo snapshotInfo)
+		{
+			return snapshotInfo;
+		}
 
-        void IEventSourcedAggregate.Persisted(Changeset changeset)
-        {
-            this.Version = changeset.AggregateVersion;
-            this.PendingChanges.Clear();
-        }
+		SnapshotInfo ISnaphottable.GetSnapshot()
+		{
+			return new SnapshotInfo(
+				this.Id,
+				this.Version,
+				this.State,
+				this.StateSignature
+			);
+		}
 
-        void IEventSourcedAggregate.ApplyChanges(Changeset changeset)
-        {
-            // skip if same version
-            if (changeset.AggregateVersion == this.Version)
-                return;
+		void IEventSourcedAggregate.Loaded()
+		{
+			PostLoadingProcessing();
+		}
 
-            if (changeset.AggregateVersion != this.Version + 1)
-                throw new AggregateRestoreException(this.Version + 1, changeset.AggregateVersion);
+		void IEventSourcedAggregate.Persisted(Changeset changeset)
+		{
+			this.Version = changeset.AggregateVersion;
+			this.PendingChanges.Clear();
+		}
 
-            this.Version = changeset.AggregateVersion;
+		void IEventSourcedAggregate.ApplyChanges(Changeset changeset)
+		{
+			// skip if same version
+			if (changeset.AggregateVersion == this.Version)
+				return;
 
-            foreach (var @event in PreprocessEvents(changeset.Events))
-            {
-                this._processor.Process(this.State, @event);
-            }
-        }
+			if (changeset.AggregateVersion != this.Version + 1)
+				throw new AggregateRestoreException(this.Version + 1, changeset.AggregateVersion);
 
-        Changeset IEventSourcedAggregate.GetChangeSet()
-        {
-            return new Changeset(
-                this.Version + 1,
-                this.PendingChanges.ToArray()
-            );
-        }
+			this.Version = changeset.AggregateVersion;
 
-        protected virtual void Track(object @event, object outcome)
-        {
-            this.PendingChanges.Add(@event);
-        }
+			foreach (var @event in PreprocessEvents(changeset.Events))
+			{
+				this._processor.Process(this.State, @event);
+			}
+		}
 
-        protected virtual IEnumerable<object> PreprocessEvents(object[] events)
-        {
-            return events;
-        }
+		Changeset IEventSourcedAggregate.GetChangeSet()
+		{
+			return new Changeset(
+				this.Version + 1,
+				this.PendingChanges.ToArray()
+			);
+		}
 
-        protected void Emit(object @event)
-        {
-            var outcome = this._processor.Process(this.State, @event);
-            Track(@event, outcome);
-        }
-    }
+		protected virtual void Track(object @event, object outcome)
+		{
+			this.PendingChanges.Add(@event);
+		}
+
+		protected virtual IEnumerable<object> PreprocessEvents(object[] events)
+		{
+			return events;
+		}
+
+		protected void Emit(object @event)
+		{
+			var outcome = this._processor.Process(this.State, @event);
+			Track(@event, outcome);
+		}
+	}
 }
