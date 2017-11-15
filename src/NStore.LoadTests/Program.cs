68c2fceb3bf18390052e64a1d8ae443775db85a8
Andea Balducci <mtb.snowboard@gmail.com>
2017-11-15 22:31:18 +0100

    added app metrics

---
+++ src/NStore.LoadTests/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using App.Metrics;
+using NStore.Core.InMemory;
+using NStore.Core.Persistence;
+
+namespace NStore.LoadTests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var metrics = new MetricsBuilder()
+                .Report.ToConsole()
+                .Build();
+
+            Track.Init(metrics);
+
+            Track.StartReporter(TimeSpan.FromSeconds(1));
+            await WriteOnStream().ConfigureAwait(false);
+            await Track.FlushReporter();
+
+            System.Console.ReadKey();
+        }
+
+        static async Task WriteOnStream()
+        {
+            var persistence = new InMemoryPersistence();
+            var payload = new byte[4096];
+
+            var worker = new ActionBlock<int>(async i =>
+                {
+                    Track.Inc(Counters.Iterations);
+                    await Task.Delay(3);
+                    await persistence.AppendAsync("test", i, payload, i.ToString())
+                        .ConfigureAwait(false);
+                },
+                new ExecutionDataflowBlockOptions()
+                {
+                    MaxDegreeOfParallelism = 40
+                }
+            );
+
+            for (int c = 1; c <= 100000; c++)
+            {
+                await worker.SendAsync(c).ConfigureAwait(false);
+            }
+
+            await worker.Completion.ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/NStore.LoadTests/Track.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Counter;
+using App.Metrics.Scheduling;
+
+namespace NStore.LoadTests
+{
+    public static class Counters
+    {
+        public static readonly CounterOptions Iterations = new CounterOptions {Name = "iterations"};
+    }
+
+    public static class Track
+    {
+        private static IMetricsRoot _metrics;
+        private static AppMetricsTaskScheduler _scheduler;
+
+        public static void Init(IMetricsRoot metrics)
+        {
+            _metrics = metrics;
+        }
+
+        public static void Inc(CounterOptions counter)
+        {
+            _metrics.Measure.Counter.Increment(counter);
+        }
+
+        public static void StartReporter(TimeSpan delay)
+        {
+            _scheduler = new AppMetricsTaskScheduler(
+                delay,
+                () => Task.WhenAll(_metrics.ReportRunner.RunAllAsync())
+            );
+            _scheduler.Start();
+        }
+
+        public static async Task FlushReporter()
+        {
+            _scheduler?.Dispose();
+            await Task.WhenAll(_metrics.ReportRunner.RunAllAsync()).ConfigureAwait(false);
+        }
+    }
+}
