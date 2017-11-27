100f4d1bbd2de054027e07e718757c724bf969f1
Andea Balducci <mtb.snowboard@gmail.com>
2017-11-27 23:00:06 +0100

    testing syntactic stuff for state handling/routing on poco aggregates

---
+++ src/NStore.Domain.Tests/PocoAggregateTests/PocoAggregateRoutingTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NStore.Domain.Tests.PocoAggregateTests
+{
+    public class TurnOn
+    {
+    }
+
+    public class TurnOff
+    {
+    }
+
+    public class SwitchedOn
+    {
+    }
+
+    public class SwitchedOff
+    {
+    }
+
+    public delegate object Executor(object command);
+
+    
+    
+    
+    public class LightBulb
+    {
+        private Executor _state;
+
+        private void TransitionTo(Executor state)
+        {
+            _state = state;
+        }
+
+        public LightBulb()
+        {
+            _state = StateOff;
+        }
+
+        private object StateOff(object command)
+        {
+            switch (command)
+            {
+                case TurnOn t: return new SwitchedOn();
+            }
+            throw new NotSupportedException();
+        }
+
+        private object StateOn(object command)
+        {
+            switch (command)
+            {
+                case TurnOff t: return new SwitchedOff();
+            }
+            throw new NotSupportedException();
+        }
+
+        public bool IsOn { get; private set; }
+
+        private void On(SwitchedOn evt)
+        {
+            this.IsOn = true;
+            TransitionTo(StateOn);
+        }
+
+        private void On(SwitchedOff evt)
+        {
+            this.IsOn = false;
+            TransitionTo(StateOff);
+        }
+
+        public object OnCommand(object command)
+        {
+            return _state(command);
+        }
+    }
+
+    public class PocoAggregateRoutingTests : AbstractPocoAggregateTest<LightBulb>
+    {
+        private IPocoAggregate LightBulb => Aggregate;
+
+        [Fact]
+        public void should_switch_on()
+        {
+            LightBulb.Do(new TurnOn());
+            Assert.True(State.IsOn);
+        }
+
+        [Fact]
+        public void should_switch_off()
+        {
+            Setup(() => { LightBulb.Do(new TurnOn()); });
+
+            LightBulb.Do(new TurnOff());
+            Assert.False(State.IsOn);
+        }
+
+        [Fact]
+        public void should_throw_if_turned_on_twice()
+        {
+            Setup(() => { LightBulb.Do(new TurnOn()); });
+
+            Assert.Throws<NotSupportedException>(() =>
+                LightBulb.Do(new TurnOn())
+            );
+        }
+    }
+}
--- src/NStore.Domain/PocoAggregate.cs
+++ src/NStore.Domain/PocoAggregate.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Reflection;
 using NStore.Core.Processing;
 
 // ReSharper disable ClassNeverInstantiated.Global
@@ -10,11 +12,78 @@ namespace NStore.Domain
         void Do(object command);
     }
 
+    public interface ICommandProcessor
+    {
+        object RunCommand(object state, object command);
+    }
+
+    public class DefaultCommandProcessor : ICommandProcessor
+    {
+        public static readonly ICommandProcessor Instance = new DefaultCommandProcessor();
+
+        private DefaultCommandProcessor()
+        {
+        }
+
+        public object RunCommand(object state, object command)
+        {
+            return state.CallPublic("Do", command);
+        }
+    }
+
+    public class OnCommandProcessor : ICommandProcessor
+    {
+        private readonly MethodInfo _onCommand;
+
+        public OnCommandProcessor(MethodInfo onCommand)
+        {
+            _onCommand = onCommand;
+        }
+
+        public object RunCommand(object state, object command)
+        {
+            try
+            {
+                return _onCommand.Invoke(state, new[] {command});
+            }
+            catch (TargetInvocationException e)
+            {
+                if (e.InnerException != null)
+                {
+                    throw e.InnerException;
+                }
+
+                throw;
+            }
+        }
+    }
+
     public class PocoAggregate<TState> : Aggregate<TState>, IPocoAggregate where TState : class, new()
     {
+        private ICommandProcessor _processor = DefaultCommandProcessor.Instance;
+
+        protected override void AfterInit()
+        {
+            base.AfterInit();
+
+            var onCommand = typeof(TState).GetMethod(
+                "OnCommand",
+                BindingFlags.Public | BindingFlags.Instance,
+                null,
+                new[] {typeof(object)},
+                null
+            );
+
+            if (onCommand != null)
+            {
+                _processor = new OnCommandProcessor(onCommand);
+            }
+        }
+
         public void Do(object command)
         {
-            var events = State.CallPublic("Do", command);
+            var events = _processor.RunCommand(State, command);
+
             if (events is IEnumerable enumerable)
             {
                 foreach (var e in enumerable)
