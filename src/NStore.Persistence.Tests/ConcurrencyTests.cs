799eefd39865e8cb758307b79b2596b7e116308c
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2017-12-27 18:20:21 +0100

    added more concurrency tests

--- src/NStore.Persistence.Tests/ConcurrencyTests.cs
+++ src/NStore.Persistence.Tests/ConcurrencyTests.cs
@@ -1,5 +1,4 @@
 ﻿using NStore.Core.Persistence;
-using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -42,6 +41,56 @@ namespace NStore.Persistence.Tests
             Assert.Equal("Duplicated index 1 on stream test", ex.Message);
             Assert.Equal("test", ex.StreamId);
             Assert.Equal(1, ex.StreamIndex);
+        }
+
+        [Fact]
+        public async Task Retrying_Write_After_a_DuplicateStreamIndexException_Should_Work()
+        {
+            // Repo1 writes to the stream (no index specified)
+            await Store.AppendAsync("test", 1).ConfigureAwait(false);
+
+            // Repo2 writes to the same stream (no index specified)
+            var ex = await Assert.ThrowsAnyAsync<DuplicateStreamIndexException>(() =>
+                Store2.AppendAsync("test", 2)
+            ).ConfigureAwait(false);
+
+            Assert.Equal("Duplicated index 1 on stream test", ex.Message);
+            Assert.Equal("test", ex.StreamId);
+            Assert.Equal(1, ex.StreamIndex);
+
+            // Repo2 retries to write the same chunk again
+            await Store2.AppendAsync("test", 2).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task Multiple_Writes_On_Same_Stream_From_Multiple_Repositories()
+        {
+            // Repo1 writes to the stream (no index specified)
+            await Store.AppendAsync("test", 1).ConfigureAwait(false);
+
+            // Repo2 writes to the same stream (no index specified)
+            var ex = await Assert.ThrowsAnyAsync<DuplicateStreamIndexException>(() =>
+                Store2.AppendAsync("test", 2)
+            ).ConfigureAwait(false);
+
+            Assert.Equal("Duplicated index 1 on stream test", ex.Message);
+            Assert.Equal("test", ex.StreamId);
+            Assert.Equal(1, ex.StreamIndex);
+
+            // Repo2 retries to write the same chunk again
+            await Store2.AppendAsync("test", 2).ConfigureAwait(false);
+
+            // Repo1 tries to wrote something again
+            await Store.AppendAsync("test", 3).ConfigureAwait(false);
+
+            // Read the data back
+            // I need an inspector to check the streams
+            var inspector = Create(false);
+            var tape = await inspector.GetAllEventForAPartition("test").ConfigureAwait(false);
+            Assert.Equal(3, tape.Length);
+            Assert.Equal(1, tape[0]);
+            Assert.Equal(2, tape[1]);
+            Assert.Equal(3, tape[2]);
         }
     }
 }
