22af9d94323050bfe0b055388e403b96e0145c17
Alkampfergit <ricci.gianmaria@gmail.com>
2018-11-26 13:13:42 +0100

    Better exception messages.

--- src/NStore.Domain/AggregateRestoreException.cs
+++ src/NStore.Domain/AggregateRestoreException.cs
@@ -2,15 +2,18 @@
 
 namespace NStore.Domain
 {
+#pragma warning disable RCS1194 // Implement exception constructors.
     public class AggregateRestoreException : Exception
     {
         public long RestoreVersion { get; }
         public long ExpectedVersion { get; }
 
-        public AggregateRestoreException(long expectedVersion, long restoreVersion)
+        public AggregateRestoreException(Type aggregateType, long expectedVersion, long restoreVersion)
+            : base($"Unable to restore aggregate {aggregateType}, expected Changeset with version {expectedVersion} but the aggregate received wrong version {restoreVersion} instead")
         {
             ExpectedVersion = expectedVersion;
             RestoreVersion = restoreVersion;
         }
     }
+#pragma warning restore RCS1194 // Implement exception constructors. 
 }
