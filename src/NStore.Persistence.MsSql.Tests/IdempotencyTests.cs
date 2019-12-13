7c067e5b3e1f82ff936db7e9439c943af67726bc
Andrea Balducci <mtb.snowboard@gmail.com>
2019-12-14 00:35:49 +0100

    added tests for idempotency opt-out

---
+++ src/NStore.Persistence.MsSql.Tests/IdempotencyTests.cs
@@ -0,0 +1,53 @@
+using System.Threading;
+using System.Threading.Tasks;
+using NStore.Core.Persistence;
+using NStore.Persistence.Tests;
+using Xunit;
+
+namespace NStore.Persistence.MsSql.Tests
+{
+    public class IdempotencyTests : BasePersistenceTest
+    {
+        protected override MsSqlPersistenceOptions CreateOptions()
+        {
+            var opts = base.CreateOptions();
+            opts.StreamIdempotencyEnabled = false;
+            return opts;
+        }
+
+        [Fact]
+        public async Task should_append_chunk_with_duplicated_operation_id()
+        {
+            var chunk1 = await Store.AppendAsync("partition", 1, "payload 1", "same_op", CancellationToken.None);
+            var chunk2 = await Store.AppendAsync("partition", 2, "payload 2", "same_op", CancellationToken.None);
+            
+            Assert.NotNull(chunk1);
+            Assert.NotNull(chunk2);
+            
+            Assert.Equal(1, chunk1.Position);
+            Assert.Equal(2, chunk2.Position);
+        }
+
+        [Fact]
+        public async Task reading_stream_by_operation_id_should_throw()
+        {
+            var ex = await Assert.ThrowsAsync<MsSqlPersistenceException>(async () =>
+            {
+                await Store.ReadByOperationIdAsync("partition", "anyop", CancellationToken.None);
+            });
+            
+            Assert.Equal("Stream idempotency is disabled. Cannot search by OperationId", ex.Message);
+        }
+        
+        [Fact]
+        public async Task reading_store_by_operation_id_should_throw()
+        {
+            var ex = await Assert.ThrowsAsync<MsSqlPersistenceException>(async () =>
+            {
+                await Store.ReadAllByOperationIdAsync("anyop", NullSubscription.Instance, CancellationToken.None);
+            });
+            
+            Assert.Equal("Stream idempotency is disabled. Cannot search by OperationId", ex.Message);
+        }
+    }
+}
