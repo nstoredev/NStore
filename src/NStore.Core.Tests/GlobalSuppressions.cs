62e36dfa22ca42efd8ac991edef2e77c5a3daeb3
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-04-26 11:48:16 +0200

    Small code cleanup.

---
+++ src/NStore.Core.Tests/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Major Code Smell",
+    "S1144:Unused private types or members should be removed",
+    Justification = "Used for testing on the On methods",
+    Scope = "module")]
